369af97ea5cf0f2fde046ada1cac87ab07e21153
tatsianagithub <tan_sham@hotmail.com>
2018-06-08 14:19:45 -0400

    test again file python

---
+++ reverse.py
@@ -0,0 +1,12 @@
+liste_etu = ["Botros", "Gabriel", "Marc Ricardo", "Jordan", "Alex", "Vincent", "Jordan"]
+
+# Que fait la méthode reverse() sur un liste ?
+# Rep: Elle inverse l'ordre des éléments dans la liste.
+
+print("La liste avant d'appliquer la méthode reverse(): ")
+print(liste_etu)
+
+liste_etu.reverse()
+
+print("\nLa liste après avoir appliqué la méthode reverse(): ")
+print(liste_etu)
